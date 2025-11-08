3f2a8f10692f28d616f1edd2b8dc5fe976d3a51a
Trust Group <EmmanuelAmemornu@technologist.com>
2025-11-08 05:58:04 -0800

    Update tech exports

--- components/technologies/index.ts
+++ components/technologies/index.ts
@@ -1,5 +1,7 @@
 export { TechnologiesSection } from "./technologies-section";
 export { TechnologyCard } from "./technology-card";
+export { PremiumTechShowcase } from "./premium-tech-showcase";
+export { TechLogo } from "./tech-logos";
 export { technologies, technologyCategories } from "./technologies-data";
 export type { Technology } from "./technologies-data";
 
