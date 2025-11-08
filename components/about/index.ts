30f9dc26439979269fcf5c28248f4e7a6d5f551e
Trust Group <EmmanuelAmemornu@technologist.com>
2025-11-08 06:09:37 -0800

    Update about section exports

--- components/about/index.ts
+++ components/about/index.ts
@@ -4,7 +4,11 @@ export { StatisticsSection } from "./statistics-section";
 export { ValuesGrid } from "./values-grid";
 export { EcosystemSection } from "./ecosystem-section";
 export { CompanyTimeline } from "./company-timeline";
+export { EnhancedTimeline } from "./enhanced-timeline";
 export { LeadershipTeam } from "./leadership-team";
+export { EnhancedTeamSection } from "./enhanced-team-section";
+export { EnhancedWhyChooseUs } from "./enhanced-why-choose-us";
+export { ValueIcon } from "./value-icons";
 export { AwardsSection } from "./awards-section";
 export { CareersTeaser } from "./careers-teaser";
 
